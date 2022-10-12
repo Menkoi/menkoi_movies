9dbb559a11ddfd4b492dd3c681120af40dd2f881
celaeon <celaeon@gmail.com>
2022-10-12 17:05:32 +0200

    added router dom, workin multi page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from'react';
+import {Route, Routes} from 'react-router-dom';
+
+import Nav from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+// Need to add Btn 3 and Btn 4
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>} />
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+export default function Nav() {
+    return(
+        <nav class="navbar navbar-inverse elegant-color">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1 text-light">
+            <Link to="/home" class="nav-link">HOME</Link>
+            <Link to="/About" class="nav-link">ABOUT</Link>
+            <Link to="/" class="nav-link">Btn 3</Link>
+            <Link to="/" class="nav-link">Btn 4</Link>
+            <Link to="/Contact" class="nav-link">CONTACT</Link>
+        </span>
+      </div>
+    </nav>
+    )
+}
